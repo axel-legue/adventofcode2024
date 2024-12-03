1e5c6e249556e51bd757e9d3c393bc013b2a163a
axel <axel.legue31@gmail.com>
2024-12-03 23:35:22 +0100

    Advent of code day 3 step 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,10 +17,26 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        return 1
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
+        var total: Int = 0
+        var shouldDoTheMultiplication = true
+        input.forEach {
+            regex.findAll(it).forEach { matchResult ->
+                when {
+                    matchResult.value.startsWith("mul") -> {
+                        val (firstNumber, secondNumber) = matchResult.groups[1]?.value?.toInt() to matchResult.groups[2]?.value?.toInt()
+                        if (firstNumber != null && secondNumber != null && shouldDoTheMultiplication) total += (firstNumber * secondNumber)
+                    }
+                    matchResult.value == "do()" -> shouldDoTheMultiplication = true
+                    matchResult.value == "don't()" -> shouldDoTheMultiplication = false
+                }
+            }
+        }
+
+        return total
     }
 
     val input = readInput("Day03")
     part1(input).println()
-    //  part2(input).println()
+    part2(input).println()
 }
